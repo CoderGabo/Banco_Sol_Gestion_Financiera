f3e92d4348ed45258ee505924994a683c14c76c9
codergabo <gabdihu@gmail.com>
2026-07-09 21:15:09 -0400

    Logd Exchange Rate

--- Banco_Sol_Gestion_Financiera/Services/ExchangeRate/ExchangeRateService.cs
+++ Banco_Sol_Gestion_Financiera/Services/ExchangeRate/ExchangeRateService.cs
@@ -5,20 +5,32 @@ namespace Banco_Sol_Gestion_Financiera.Services.ExchangeRate
     public class ExchangeRateService : IExchangeRateService
     {
         private readonly HttpClient _httpClient;
+        private readonly ILogger<ExchangeRateService> _logger;
 
-        public ExchangeRateService(HttpClient httpClient)
+        public ExchangeRateService(
+            HttpClient httpClient,
+            ILogger<ExchangeRateService> logger
+        )
         {
             _httpClient = httpClient;
+            _logger = logger;
         }
 
         public async Task<ExchangeRateDto> GetCurrentRateAsync()
         {
+            _logger.LogInformation("Consultando HexaRate");
             var response = await _httpClient.GetAsync("rates/USD/BOB/latest");
 
+            _logger.LogInformation("Status: {Status}", response.StatusCode);
             response.EnsureSuccessStatusCode();
+
+            var body = await response.Content.ReadAsStringAsync();
+
+            _logger.LogInformation("Body: {Body}", body);
 
             var result = await response.Content.ReadFromJsonAsync<HexaRateResponseDto>();
 
+            
             if (result?.Data is null)
                 throw new Exception("No fue posible obtener el tipo de cambio.");
 
